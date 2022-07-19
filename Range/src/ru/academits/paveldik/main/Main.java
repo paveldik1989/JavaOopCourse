ed7b4cfa86237aaabcbbd90aa3ea6cf8d421f100
paveldik1989 <paveldik1989@gmail.com>
2022-07-20 02:09:08 +0700

    Project HelloGit has been added.

---
+++ Range/src/ru/academits/paveldik/main/Main.java
@@ -0,0 +1,21 @@
+package ru.academits.paveldik.main;
+
+import ru.academits.paveldik.range.Range;
+
+public class Main {
+    public static void main(String[] args) {
+        Range range1 = new Range(1, 2);
+
+        System.out.println(range1.getFrom());
+        System.out.println(range1.getTo());
+        System.out.println(range1.getLength());
+        System.out.println(range1.isInside(1.5));
+
+        range1.setTo(9);
+        range1.setFrom(7);
+
+        System.out.println(range1.getFrom());
+        System.out.println(range1.getTo());
+        System.out.println(range1.isInside(1.5));
+    }
+}

833a3f3dd84a354bc798c0e9be7c64bac497303b
paveldik1989 <paveldik1989@gmail.com>
2022-08-16 01:08:52 +0700

    Class Main in module Range has been revised.

--- Range/src/ru/academits/paveldik/range_main/Main.java
+++ Range/src/ru/academits/paveldik/range_main/Main.java
@@ -2,6 +2,8 @@ package ru.academits.paveldik.range_main;
 
 import ru.academits.paveldik.range.Range;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Range range1 = new Range(1, 4);
@@ -12,29 +14,13 @@ public class Main {
 
         System.out.println("Пересечение: " + range1.getIntersection(range2));
 
-        Range[] union = range1.getUnion(range2);
-        System.out.println("Объединение: " + rangesArrayToString(range1.getUnion(range2)));
+        System.out.println("Объединение: " + convertRangesArrayToString(range1.getUnion(range2)));
 
-        Range[] difference1 = range1.getDifference(range2);
-        System.out.println("Вычитание 1 - 2: " + rangesArrayToString(range1.getDifference(range2)));
-
-        Range[] difference2 = range2.getDifference(range1);
-        System.out.println("Вычитание 2 - 1: " + rangesArrayToString(range2.getDifference(range1)));
+        System.out.println("Вычитание 1 - 2: " + convertRangesArrayToString(range1.getDifference(range2)));
+        System.out.println("Вычитание 2 - 1: " + convertRangesArrayToString(range2.getDifference(range1)));
     }
 
-    public static String rangesArrayToString(Range[] ranges) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append('[');
-
-        if (ranges.length > 0) {
-            for (Range range : ranges) {
-                stringBuilder.append(range).append(", ");
-            }
-
-            stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
-        }
-
-        stringBuilder.append(']');
-        return stringBuilder.toString();
+    public static String convertRangesArrayToString(Range[] ranges) {
+        return Arrays.toString(ranges);
     }
 }
